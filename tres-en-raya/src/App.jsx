d6b18547296c81b7c94a1988c124e6e9934cf339
Jesús Alarcón <jesus.alarconfrias@hotmail.com>
2023-09-05 22:32:20 -0600

    board and show who it is turn

--- tres-en-raya/src/App.jsx
+++ tres-en-raya/src/App.jsx
@@ -1,36 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+const turns = {
+  X: "x",
+  O: "o",
+};
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+  return <div className={className}>{children}</div>;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(turns.X);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more.
-        This is a new test to see if git works
-      </p>
-    </>
-  )
+    <main className="board">
+      <h1>Tic tac toe</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <Square key={index} index={index}>
+              {board[index]}
+            </Square>
+          );
+        })}
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === turns.X}>{turns.X}</Square>
+        <Square isSelected={turn === turns.O}>{turns.O}</Square>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
